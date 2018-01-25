7d2fc188a516811f697b60319a8f098497b3c26d
Chris Nevin <cjnevin@gmail.com>
2018-01-25 22:37:00 +0000

    Added ability to add associated presenters to enable stacked behaviour.

--- RxMvpExample/Presentation/Presentation/Presenter.swift
+++ RxMvpExample/Presentation/Presentation/Presenter.swift
@@ -5,18 +5,26 @@ public class Presenter<T> {
     public typealias View = T
     private var disposeBag = DisposeBag()
     private var view: View?
-
+    private var subPresenters: [Presenter<T>] = []
+    
+    public func attachPresenter(_ presenter: Presenter<T>) {
+        subPresenters.append(presenter)
+    }
+    
     public func attachView(_ newView: View) {
         assert(view == nil)
         view = newView
+        subPresenters.forEach { $0.attachView(newView) }
     }
 
     public func detachView() {
+        subPresenters.forEach { $0.detachView() }
         view = nil
         disposeBag = DisposeBag()
     }
 
     public func disposeOnViewDetach(_ disposable: Disposable) {
+        subPresenters.forEach { $0.disposeOnViewDetach(disposable) }
         disposeBag.insert(disposable)
     }
 }
